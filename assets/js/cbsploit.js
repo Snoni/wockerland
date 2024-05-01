6c894e20f2527fdc9d0d742e8af1f2834bce5d12
Wocky <60744280+Snoni@users.noreply.github.com>
2024-05-01 03:54:25 +0000

    aaaaaaaaa

--- assets/js/cbsploit.js
+++
@@ -1,9 +0,0 @@
-alert("exploiting chromebook (may freeze for a bit)") 
-while(true){     
-onbeforeunload=function() {
-      localstorage.x=1
-    };
-    setTimeout(function() {
-      while (1) location.reload(1)
-    }, 1000);
-}
